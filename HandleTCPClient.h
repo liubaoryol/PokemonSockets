2c63fe720dbad6b9c837db0d1ff78008ac37f87d
Alexis Rodriguez <jap.chan@ciencias.unam.mx>
2017-12-19 19:22:32 -0600

    Testeando respuesta del servidor

--- HandleTCPClient.h
+++ HandleTCPClient.h
@@ -1,3 +1,37 @@
+#include <stdio.h>             /* for printf() and fprintf() */
+#include <sys/socket.h>        /* for recv() and send() */
+#include <unistd.h>            /* for close() */
+#include "MessageDefinition.h" /* structures for messages */
+
+#define RCVBUFSIZE 32     
+
+void DieWithError(char *errorMessage);            /* Error handling function */
+
 void HandleTCPClient(int clntSocket){
+
+  char buffer[RCVBUFSIZE];     /* buffer for echo string */
+  int recvMsgSize;             /* Size of received message */
+  unsigned short code;         /* Code for the message */
+  char serverResponse[22];     /* for store confirmation message */
+
+  strcpy(serverResponse, "Data has been received");
+
+  if((recvMsgSize = recv(clntSocket, buffer, RCVBUFSIZE, 0)) < 0)
+    DieWithError("recv() failed");
+
+  while(recvMsgSize > 0){
+    
+    /* Confirmation message back to client */
+    if(send(clntSocket, serverResponse, sizeof(serverResponse), 0) < 0)
+      DieWithError("recv() failed");
+
+    /* See if there is more data to receive */
+    if((recvMsgSize = recv(clntSocket, buffer, RCVBUFSIZE, 0)) < 0)
+      DieWithError("recv() failed");
+    
+  }
+
+  printf("Data Received");
+  close(clntSocket);    /* Close client socket */
   
 };
