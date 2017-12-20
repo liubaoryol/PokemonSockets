02ce8b433b7b5da50e2823a727787ed8e97a313c
Luka <queti>
2017-12-19 18:40:03 -0600

    Ahora si la buena

---
+++ MessageDefinition.c
@@ -0,0 +1,5 @@
+/*Estructura para los mensajes 10,30,31,32 y 23*/
+typedef struct msg_type1
+{
+	unsigned char code[1];
+}msg_type1;
---
+++ PokemonClient.c
@@ -0,0 +1,68 @@
+#include <stdio.h>          /* for printf() and fprintf() */
+#include <sys/socket.h>     /* for socket(), bind() and connect() */
+#include <arpa/inet.h>      /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>         /* for atoi() */
+#include <string.h>         /* for memset() */
+#include <unistd.h>         /* for close() */
+#include <pthread.h>         /* for POSIX threads */
+#include "DieWithError.h"  /* error handling function */
+#include "MassageDefinition.h"
+
+#define SERV_PORT 9999            /* Server will run on this port */
+#define RCVBUFSIZE 32
+
+void DieWithError(char *errorMessage); /*Error handling function*/
+
+int main(int argc, char *argv[])
+{
+	int sock;
+	struct sockaddr_in pokeServAddr;
+	unsigned short servPort;
+	char *servIP;
+	char *pokeString;
+	char pokeBuffer[RCVBUFSIZE];
+	unsigned int pokeStringLen;
+	int bytesRcvd, bytesSend;
+
+	unsigned int code = 10;
+
+	if((argc != 3)){ /*temp*/
+		fprintf(stderr, "Usage: %s <ServerIP> <Port>\n",argv[0]);
+		exit(1);
+	}
+
+	servIP = argv[1];
+	servPort = atoi(argv[2]); /*cast String to int*/
+
+	/*CREATING MSG STRUCTURE*/
+	msg_type1 *msg_t1 = (struct msg_type1 *)(unsigned char *)malloc(sizeof (unsigned char))
+	
+	memcpy(msg_t1->code, &code, sizeof(msg_t1->code));
+
+	/*CREATING SOCKET*/
+	if((sock=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		DieWithError("socket () failed");
+
+	memset(&pokeServAddr,0,sizeof(pokeServAddr));
+
+	pokeServAddr.sin_family = AF_INET;
+	pokeServAddr.sin_addr.s_addr = inet_addr(servIP);
+
+	pokeServAddr.sin_port = htons(servPort);
+
+	/*CONNECTING SOCKET*/
+	if((connect(sock,(struct sockaddr *)&pokeServAddr, sizeof(pokeServAddr))) < 0)
+		DieWithError("connect () failed");
+
+	pokeStringLen = sizeof(msg_type1);
+
+	/*SENDING MSG*/
+	if((bytesSend = send(sock, msg_t1, sizeof(msg_type1), 0)) != pokeStringLen)
+		DieWithError("send() sent a different number of bytes than expected");
+	else
+		printf("Bytes send to server: %i \n",bytesSend);
+
+	close(sock);
+
+	exit(0);
+}
